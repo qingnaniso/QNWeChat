8d290291677315fe71548c602322756575c5e5c5
qiqingnan <qiqingnan@126.com>
2016-06-16 16:31:27 +0800

    add AddressBookViewController

---
+++ QNWeChat/src/addressbook/controller/QNAddressBookViewController.h
@@ -0,0 +1,13 @@
+//
+//  QNAddressBookViewController.h
+//  QNWeChat
+//
+//  Created by smartrookie on 16/6/16.
+//  Copyright © 2016年 smartrookie. All rights reserved.
+//
+
+#import "QNBaseViewController.h"
+
+@interface QNAddressBookViewController : QNBaseViewController
+
+@end
---
+++ QNWeChat/src/addressbook/controller/QNAddressBookViewController.m
@@ -0,0 +1,38 @@
+//
+//  QNAddressBookViewController.m
+//  QNWeChat
+//
+//  Created by smartrookie on 16/6/16.
+//  Copyright © 2016年 smartrookie. All rights reserved.
+//
+
+#import "QNAddressBookViewController.h"
+
+@interface QNAddressBookViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation QNAddressBookViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
